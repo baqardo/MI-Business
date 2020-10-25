d871792490794d784862e11ad6a03f70a9053d2f
Andrzej Nawalaniec <46224560+baqardo@users.noreply.github.com>
2020-10-25 20:17:09 +0100

    Responsive navigation

---
+++ js/mobileNavigation.js
@@ -0,0 +1,43 @@
+const burger = document.querySelector('.burger');
+
+const mobileNavHandler = () => {
+  let isNavInvisible = true;
+  const nav = document.querySelector('.js-nav');
+  const mobileNav = document.querySelector('.js-home__nav-mobile');
+  const blur = document.querySelector('.js-blur');
+  const burgerToggler = document.querySelector('.js-burger-toggler');
+
+  console.log(nav, mobileNav);
+
+  burger.addEventListener('click', () => {
+    if (isNavInvisible) {
+      mobileNav.classList.add('home__nav-mobile-is-visible');
+      blur.classList.add('blur__is-visible');
+    } else {
+      mobileNav.classList.remove('home__nav-mobile-is-visible');
+      blur.classList.remove('blur__is-visible');
+    }
+
+    isNavInvisible = !isNavInvisible;
+  });
+
+  blur.addEventListener('click', () => {
+    mobileNav.classList.remove('home__nav-mobile-is-visible');
+    blur.classList.remove('blur__is-visible');
+
+    burgerToggler.checked = false;
+    isNavInvisible = !isNavInvisible;
+  });
+
+  [nav, mobileNav].forEach(element => {
+    element.addEventListener('click', event => {
+      if (!isNavInvisible && event.target.matches('.link')) {
+        mobileNav.classList.remove('home__nav-mobile-is-visible');
+        blur.classList.remove('blur__is-visible');
+
+        burgerToggler.checked = false;
+        isNavInvisible = !isNavInvisible;
+      }
+    });
+  });
+};
