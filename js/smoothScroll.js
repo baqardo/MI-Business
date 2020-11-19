32a2a567ad42efb22409f2b794b0417446658df6
Andrzej Nawalaniec <46224560+baqardo@users.noreply.github.com>
2020-11-19 21:22:54 +0100

    Bug fixes

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,17 +1,17 @@
 const smoothScroll = () => {
   gsap.registerPlugin(ScrollToPlugin);
 
-  const page = document.querySelector(".l-page");
+  const page = document.querySelector('.l-page');
 
-  page.addEventListener("click", event => {
-    const targetAnchor = event.target.closest("a.js-anchor");
+  page.addEventListener('click', event => {
+    const targetAnchor = event.target.closest('a.js-anchor');
     if (!targetAnchor) return;
 
     event.preventDefault();
 
-    const targetSection = targetAnchor.href.substring(targetAnchor.href.lastIndexOf("#"));
+    const targetSection = targetAnchor.href.substring(targetAnchor.href.lastIndexOf('#'));
     if (targetSection.length < 2) return;
 
-    gsap.to(window, { duration: 1.5, scrollTo: targetSection, ease: "power2.inOut" });
+    gsap.to(window, { duration: 1.5, scrollTo: targetSection, ease: 'power1.in' });
   });
 };
